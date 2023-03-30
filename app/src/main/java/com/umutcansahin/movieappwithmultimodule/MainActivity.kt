1168c3aecf52bfbb9b88d6704ba4957b5131b82a
Umutcan Sahin <umutcan199726@gmail.com>
2023-03-30 16:12:44 +0300

    add navigation module

--- app/src/main/java/com/umutcansahin/movieappwithmultimodule/MainActivity.kt
+++ app/src/main/java/com/umutcansahin/movieappwithmultimodule/MainActivity.kt
@@ -2,10 +2,25 @@ package com.umutcansahin.movieappwithmultimodule
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.umutcansahin.movieappwithmultimodule.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        NavigationUI.setupWithNavController(binding.bottomNavigationView,navController)
     }
 }
--- presentation/src/main/java/com/umutcansahin/presentation/home_screen/HomeFragment.kt
+++ presentation/src/main/java/com/umutcansahin/presentation/home_screen/HomeFragment.kt
@@ -6,27 +6,35 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.umutcansahin.presentation.R
+import com.umutcansahin.presentation.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = HomeFragment()
-    }
-
-    private lateinit var viewModel: HomeViewModel
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvTitle.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToHomeDetailFragment())
+          //  findNavController().navigate(com.umutcansahin.navigation.R.id.action_homeFragment_to_homeDetailFragment)
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
